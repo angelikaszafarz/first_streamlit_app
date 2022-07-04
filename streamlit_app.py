4eb52e8585ac340be836cbcd0eff7f9163ae579a
angelikaszafarz <52421359+angelikaszafarz@users.noreply.github.com>
2022-07-04 20:35:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,27 +24,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + #"kiwi"
+      fruit_choice)
+      #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the response and normalize it
+      return fruityvice_normalized
+
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-      fruit_choice = streamlit.text_input('What fruit would you like information about?'
-#,'Kiwi'
-                                   )
-#streamlit.write('The user entered ', fruit_choice)
+      fruit_choice = streamlit.text_input('What fruit would you like information about?' #,'Kiwi'
+                                          )
+      #streamlit.write('The user entered ', fruit_choice)
       if not fruit_choice:
             streamlit.error("Please select a fruit to get information.")
       else:
-#import requests
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +
-#"kiwi"
-            fruit_choice)
-#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-#take the json version of the response and normalize it
-            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it in the screen as a table
-            streamlit.dataframe(fruityvice_normalized)
+           back_from_function = get_fruityvice_data(fruit_choice)
+           streamlit.dataframe(back_from_function)
 except URLError as e:
       streamlit.error()
+
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
